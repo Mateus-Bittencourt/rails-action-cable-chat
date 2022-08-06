f91ad4ce25eb69036d437c68e66b8bdf2c96022a
Mateus Bittencourt <mateusmbittencourt@gmail.com>
2022-08-06 18:46:26 -0300

    add disconnect function

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -3,15 +3,20 @@ import { createConsumer } from "@rails/actioncable";
 
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
-  static values = { chatroomId: Number }
+  static values = { chatroomId: Number };
   connect() {
     createConsumer().subscriptions.create(
-      {channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
       {
-      received: (message) => {
-        this.element.insertAdjacentHTML("beforeend", message);
-        this.element.scrollTop = this.element.scrollHeight
-      },
-    });
+        received: (message) => {
+          this.element.insertAdjacentHTML("beforeend", message);
+          this.element.scrollTop = this.element.scrollHeight;
+        },
+      }
+    );
+  }
+
+  disconnect() {
+    this.channel.unsubscribe();
   }
 }
